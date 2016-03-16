2e7a2b7c7a0da2faa7da77a07f9331abc3c27d61
hisland <hisland@qq.com>
2016-03-16 15:13:38 +0800

    0.0.4

--- src/main.js
+++ src/main.js
@@ -1,14 +1,21 @@
-export function treeWalkDeep(parent, childrenKey, fn, __lv) {
-  __lv = __lv || 0;
+function treeWalkDeepInner(parent, fn, childrenKey, __lv) {
   for (var i = 0, item, len = parent[childrenKey].length; i < len; i++) {
     item = parent[childrenKey][i];
-    fn(item, i, parent, __lv)
+    fn(item, i, parent, __lv);
     if (item[childrenKey]) {
-      treeWalkDeep(item, childrenKey, fn, __lv + 1)
+      treeWalkDeepInner(item, fn, childrenKey, __lv + 1);
     }
   }
 }
-export function treeWalkParallel(parent, childrenKey, fn, __lv) {
+
+export function treeWalkDeep(parent, fn, childrenKey = 'children') {
+  treeWalkDeepInner(parent, fn, childrenKey, 0);
+}
+
+
+
+
+function treeWalkParallel(parent, childrenKey, fn, __lv) {
   __lv = __lv || 0;
   for (var i = 0, item, len = parent[childrenKey].length; i < len; i++) {
     item = parent[childrenKey][i];
@@ -17,4 +24,25 @@ export function treeWalkParallel(parent, childrenKey, fn, __lv) {
       treeWalkParallel(item, childrenKey, fn, __lv + 1)
     }
   }
+}
+
+function mapTree(orgParent, key, fn, __lv, rsParent) {
+  rsParent = rsParent || {};
+
+  var rs, len = orgParent[key].length;
+  if (len) {
+    rsParent[key] = [];
+  }
+
+  __lv = __lv || 0;
+  for (var i = 0, item; i < len; i++) {
+    item = orgParent[key][i];
+    rs = fn(item, i, orgParent, __lv);
+    rsParent[key].push(rs);
+    if (item[key]) {
+      mapTree(item, key, fn, __lv + 1, rs);
+    }
+  }
+
+  return rsParent;
 }
