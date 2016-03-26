f58ff599be5775c62f86c7756e048bff2e10cd61
hisland <hisland@qq.com>
2016-03-26 14:45:12 +0800

    rollup: out file in dest, not lib

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import commonjs from 'rollup-plugin-commonjs';
 
 export default {
   entry: 'src/main.js',
-  dest: 'lib/bundle.js',
+  dest: 'dest/bundle.js',
   format: 'cjs',
   plugins: [
     babel(),
