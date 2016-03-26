419139d6ac9e4023a47ea9e69d063581e4d6c51b
hisland <hisland@qq.com>
2016-03-26 14:43:40 +0800

    config file change

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,17 @@
 import babel from 'rollup-plugin-babel';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 
 export default {
   entry: 'src/main.js',
   dest: 'lib/bundle.js',
   format: 'cjs',
-  plugins: [babel({
-    babelrc: false,
-    presets: ["es2015-rollup"]
-  })]
+  plugins: [
+    babel(),
+    nodeResolve({
+      jsnext: true,
+      main: true,
+    }),
+    commonjs(),
+  ]
 };
